2238c7849228407bf3396373f332b38c5c895c66
rachsg7 <sch19051@byui.edu>
2021-04-29 23:16:00 -0700

    commit

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,9 +3,15 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.render('pages', { 
-        title: 'Team Activity 02', 
+const userArray = ['Peter', 'James', 'John'];
+
+router.post('/addUser', (req, res, next) => {
+
+});
+
+router.get('/', (req, res, next) => {
+    res.render('pages', {
+        title: 'Team Activity 02',
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
